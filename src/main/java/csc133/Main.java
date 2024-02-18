70e6963186e731fa449e2cd90ff9968eae38be72
rahulio96 <rahulgithub96@gmail.com>
2024-02-17 18:47:58 -0800

    Initial commit

---
+++ src/main/java/csc133/Main.java
@@ -0,0 +1,61 @@
+package csc133;
+
+
+public class Main {
+    private static slGoLBoard my_board;
+    private static final int ROWS = 7, COLS = 9;
+    public static void main(String[] args) {
+
+        test_1();
+        test_2();
+        test_3();
+        test_4();
+        test_5();
+    }
+
+    // print TwoDegreeNeighbors(0,0)
+    private static void test_1() {
+        
+        return;
+    }
+
+    // print TwoDegreeNeighbors(ROWS-1, COLS-1)
+    private static void test_2() {
+        /*
+        my_board = new slGoLBoardLive(ROWS, COLS);
+        my_board.printGoLBoard();
+        int my_row = ROWS-1, my_col = COLS-1;
+        System.out.println("TwoDegreeNeighbors(" + my_row +", " + my_col + ") --> " +
+                my_board.countLiveTwoDegreeNeighbors(ROWS-1, COLS-1));
+        System.out.println();
+         */
+        return;
+    }
+
+    // print TwoDegreeNeighbors((int)(ROWS/2), (int)(ROWS/2))
+    private static void test_3() {
+        
+        return;
+    }
+
+    // print TwoDegreeNeighbors(0, (int)(ROWS/2))
+    private static void test_4() {
+        
+        return;
+    }
+
+    // print the board and the updated board
+    private static void test_5() {
+        
+        return;
+    }
+
+}
+
+
+
+
+
+
+
+
