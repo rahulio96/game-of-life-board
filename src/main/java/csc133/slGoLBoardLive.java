50a25551274b740ff84fa8f070e44282db190576
rahulio96 <rahulgithub96@gmail.com>
2024-02-17 23:39:09 -0800

    Added given updateNextCell function

--- src/main/java/csc133/slGoLBoardLive.java
+++ src/main/java/csc133/slGoLBoardLive.java
@@ -24,6 +24,34 @@ public class slGoLBoardLive extends slGoLBoard {
     }
     @Override
     int updateNextCellArray() {
-        return -1;
-    }
+        int retVal = 0;
+
+        int nln = 0;  // Number Live Neighbors
+        boolean ccs = true; // Current Cell Status
+        for (int row = 0; row < NUM_ROWS; ++row){
+            for (int col = 0; col < NUM_COLS; ++col) {
+                ccs = liveCellArray[row][col];
+                nln = countLiveTwoDegreeNeighbors(row, col);
+                if (!ccs && nln == 3) {
+                    nextCellArray[row][col] = true;
+                    ++retVal;
+                } else {
+                    // Current Cell Status is true
+                    if (nln < 2 || nln > 3) {
+                        nextCellArray[row][col] = false;
+                    } else {
+                        // nln == 2 || nln == 3
+                        nextCellArray[row][col] = true;
+                        ++retVal;
+                    }
+                }
+            }  // for (int row = 0; ...)
+        }  //  for (int col = 0; ...)
+
+        boolean[][] tmp = liveCellArray;
+        liveCellArray = nextCellArray;
+        nextCellArray = tmp;
+
+        return retVal;
+    }  //  int updateNextCellArray()
 }
