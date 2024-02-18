9ba8bf2beba52389b7b8d6e9abef0de0f15d70c3
rahulio96 <rahulgithub96@gmail.com>
2024-02-17 23:38:33 -0800

    Updated countLive function to work for interior cells

--- src/main/java/csc133/slGoLBoardLive.java
+++ src/main/java/csc133/slGoLBoardLive.java
@@ -10,8 +10,17 @@ public class slGoLBoardLive extends slGoLBoard {
     }
 
     @Override
-    int countLiveTwoDegreeNeighbors(int row, int col) {
-        return -1;
+    public int countLiveTwoDegreeNeighbors(int row, int col) {
+        int count = 0;
+        int [][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+        for (int i = 0; i < directions.length; i++) {
+            int new_r = row + directions[i][0], new_c = col + directions[i][1];
+            // Interior cells
+            if (new_r >= 0 && new_r < NUM_ROWS && new_c >= 0 && new_c < NUM_COLS) {
+                count++;
+            }
+        }
+        return count;
     }
     @Override
     int updateNextCellArray() {
