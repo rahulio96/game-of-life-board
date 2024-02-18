c18f280bc10e416f582032f4a93c1ba85d74657c
rahulio96 <rahulgithub96@gmail.com>
2024-02-18 12:53:44 -0800

    Finished countTwoDegree tests 1-4

--- src/main/java/csc133/Main.java
+++ src/main/java/csc133/Main.java
@@ -4,7 +4,7 @@ package csc133;
 public class Main {
     //private static slGoLBoard my_board;
     private static final int ROWS = 6, COLS = 6; // 7, 9
-    private static slGoLBoardLive my_board = new slGoLBoardLive(ROWS, COLS);
+    private static final slGoLBoardLive my_board = new slGoLBoardLive(ROWS, COLS);
     public static void main(String[] args) {
 
         test_1();
@@ -14,44 +14,38 @@ public class Main {
         test_5();
     }
 
+    private static void printCountTwoDegreeNeighbors(int x, int y) {
+        my_board.printGoLBoard();
+        System.out.println("TwoDegreeNeighbors(" + x +", " + y + ") --> " +
+                my_board.countLiveTwoDegreeNeighbors(x, y));
+        System.out.println();
+    }
+
     // print TwoDegreeNeighbors(0,0)
     private static void test_1() {
-        //my_board = new slGoLBoardLive(ROWS, COLS);
-        my_board.printGoLBoard();
-        int my_row = ROWS-1, my_col = COLS-1;
-        System.out.println("TwoDegreeNeighbors(" + my_row +", " + my_col + ") --> " +
-                my_board.countLiveTwoDegreeNeighbors(0, 0));
-        System.out.println();
-        
-        return;
+        printCountTwoDegreeNeighbors(0, 0);
     }
 
     // print TwoDegreeNeighbors(ROWS-1, COLS-1)
     private static void test_2() {
-        //my_board = new slGoLBoardLive(ROWS, COLS);
-        my_board.printGoLBoard();
-        int my_row = ROWS-1, my_col = COLS-1;
-        System.out.println("TwoDegreeNeighbors(" + my_row +", " + my_col + ") --> " +
-                my_board.countLiveTwoDegreeNeighbors(ROWS-1, COLS-1));
-        System.out.println();
-        return;
+        printCountTwoDegreeNeighbors(ROWS-1, COLS-1);
     }
 
     // print TwoDegreeNeighbors((int)(ROWS/2), (int)(ROWS/2))
     private static void test_3() {
-        
-        return;
+        printCountTwoDegreeNeighbors((int)(ROWS/2), (int)(ROWS/2));
     }
 
     // print TwoDegreeNeighbors(0, (int)(ROWS/2))
     private static void test_4() {
-        
-        return;
+        printCountTwoDegreeNeighbors(0, (int)(ROWS/2));
     }
 
     // print the board and the updated board
     private static void test_5() {
-        
+        my_board.printGoLBoard();
+        System.out.println(my_board.countLiveTwoDegreeNeighbors(1, 1));
+        System.out.println();
         return;
     }
 
